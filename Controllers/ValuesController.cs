73820545b11e9fb69185de447963f0b3c51d3822
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2018-09-07 14:45:58 +0200

    refactor: Changed the namespace

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace client_api.Controllers
+namespace AppHospital.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace client_api
+namespace AppHospital.Api
 {
     public class Program
     {

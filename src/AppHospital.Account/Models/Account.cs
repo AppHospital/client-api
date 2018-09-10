5778bc058367cac130474829a39e8a0dc5ec5d90
COUSIN NICOLAS <nicolas.cousin.lbc@axa.fr>
2018-09-10 17:40:54 +0200

    feature(Account): Model initialization

---
+++ src/AppHospital.Account/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AppHospital.Account.Models
+{
+    public class Account
+    {
+        public Account(string firstname, string lastname, Squad squad)
+        {
+            Firstname = firstname ?? string.Empty;
+            Lastname = lastname ?? string.Empty;
+            Squad = squad ?? Squad.Empty;
+        }
+
+        public string Firstname { get; }
+        
+        public string Lastname { get; }
+
+        public Squad Squad { get; }
+    }
+}

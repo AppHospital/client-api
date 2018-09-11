59ac1d1eb380f3261367e58a1db58c4ddaa0c260
Nicolas Cousin <niccoudev@gmail.com>
2018-09-11 16:14:06 +0200

    feature(Swagger): Added Swagger to project

---
+++ src/AppHospital.Api/Configuration/SwaggerIServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace AppHospital.Api.Configuration
+{
+    internal static class SwaggerIServiceCollectionExtensions
+    {
+        public static string Title => "AppHospital Api";
+        
+        public static string Name => "AppHospital_Api";
+
+        public static IServiceCollection AddSwaggerApi(this IServiceCollection servicesCollection)
+        {
+            servicesCollection.AddSwaggerGen(config => 
+                config.SwaggerDoc(
+                    Name,
+                    new Info {
+                        Title = Title
+                        }));
+
+            return servicesCollection;
+        }
+    }
+}

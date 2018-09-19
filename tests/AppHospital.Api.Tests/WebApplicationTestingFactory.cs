127cb9713eb1b1e95239cab96410fdc75640a652
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2018-09-19 15:51:39 +0200

    feat(user): endpoint to get the user profile

---
+++ tests/AppHospital.Api.Tests/WebApplicationTestingFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AppHospital.Api.Tests
+{
+    public class WebApplicationTestingFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Test");
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
